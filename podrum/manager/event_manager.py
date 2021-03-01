be38fa150a52564ce417d0923fa68531e05c31fb
plun1331 <plun1331@gmail.com>
2021-02-28 17:59:53 -0800

    Modify event manager as well as implement it

--- podrum/manager/event_manager.py
+++ podrum/manager/event_manager.py
@@ -33,21 +33,19 @@ class event_manager:
     def __init__(self, server):
         self.server = server
         self.events = {}
-        
+
     def register(self, function, event: str) -> None:
         if event in self.events:
             self.events[event].append(function)
         else:
             self.events[event]: list = [function]
-    
-    def dispatch(self, event: str, *args) -> None:
-        if event in self.events:
-            for function in self.events[event]:
-                function(*args)
-            
-    def remove(self, function, event: str) -> None:
-        if event in self.events:
-            if function in self.events[name]:
-                self.events[event].remove(function)
-                if len(self.events[event]) == 0:
-                    del self.events[event]
+
+    def dispatch(self, event, *args) -> None:
+        for function in self.events.get(event, []):
+            function(*args)
+
+    def remove(self, function, event):
+        if function in self.events.get(event, []):
+            self.events[event].remove(function)
+            if not self.events[event]:
+                del self.events[event]
